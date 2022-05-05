af5d7221f21d2415b2cef95af91a216cc566b28f
Trac3r <kamilg77@interia.pl>
2022-05-05 21:14:52 +0200

    Added TController
    * integrated tangibleController
    * improved main script: net_.m - reads net from mat file
    * and send message to app when image is classified above certain threshold

--- net__.m
+++ net__.m
@@ -1,70 +1,84 @@
+% prepare input - we use laptop camera
 camera = webcam;
-net = googlenet;
 
+% load pretrained net
+% data = load("pretrainedNet.m");
+% net = data.net;
+net = googlenet; % temporary solution
+
+% define input size - images need to be resized
 inputSize = net.Layers(1).InputSize(1:2);
 
-% single
-figure
-im = snapshot(camera);
-image(im)
-im = imresize(im,inputSize);
-[label,score] = classify(net,im);
-title({char(label),num2str(max(score),2)});
+% Properties
+THRESHOLD = 0.7; % minimal score to send msg (and showe title)
+DEBUG = false; % plot extra information about detected classes
 
-% dtp
+% prepare controler with (for now) default values
+ipAddress='127.0.0.1';
+port=55001;
+debugMode = true;
+tc = TangibleController(debugMode, ipAddress, port);
+
+% prepare display
 h = figure;
-h.Position(3) = 2*h.Position(3);
-ax1 = subplot(1,2,1);
-ax2 = subplot(1,2,2);
-
-im = snapshot(camera);
-image(ax1,im)
-im = imresize(im,inputSize);
-[label,score] = classify(net,im);
-title(ax1,{char(label),num2str(max(score),2)});
-
-[~,idx] = sort(score,'descend');
-idx = idx(5:-1:1);
-classes = net.Layers(end).Classes;
-classNamesTop = string(classes(idx));
-scoreTop = score(idx);
-
-barh(ax2,scoreTop)
-xlim(ax2,[0 1])
-title(ax2,'Top 5')
-xlabel(ax2,'Probability')
-yticklabels(ax2,classNamesTop)
-ax2.YAxisLocation = 'right';
-
-% continous
-h = figure;
-h.Position(3) = 2*h.Position(3);
-ax1 = subplot(1,2,1);
-ax2 = subplot(1,2,2);
-ax2.PositionConstraint = 'innerposition';
-
+if DEBUG
+    h.Position(3) = 2*h.Position(3);
+    ax1 = subplot(1,2,1);
+    ax2 = subplot(1,2,2);
+    ax2.PositionConstraint = 'innerposition';
+end
 while ishandle(h)
     % Display and classify the image
-    im = snapshot(camera);
-    image(ax1,im)
-    im = imresize(im,inputSize);
-    [label,score] = classify(net,im);
-    title(ax1,{char(label),num2str(max(score),2)});
+    img = snapshot(camera);
 
-    % Select the top five predictions
-    [~,idx] = sort(score,'descend');
-    idx = idx(3:-1:1);
-    scoreTop = score(idx);
-    classNamesTop = string(classes(idx));
+    if DEBUG
+        image(ax1, img)
+    else
+        image(img)
+    end
+    
+    img = imresize(img, inputSize);
 
-    % Plot the histogram
-    barh(ax2,scoreTop)
-    title(ax2,'Top 5')
-    xlabel(ax2,'Probability')
-    xlim(ax2,[0 1])
-    yticklabels(ax2,classNamesTop)
-    ax2.YAxisLocation = 'right';
+    [label,score] = classify(net, img);
+
+    maxScore = max(score);
+    detectedClass = char(label);
+
+    if maxScore > THRESHOLD
+        if DEBUG
+            title(ax1,{detectedClass, num2str(maxScore, "%.2f")});
+        else
+            title({detectedClass, num2str(maxScore, 2)});
+        end
+        
+        % send message with detected class name
+        tc.ObjectDetected(detectedClass);
+    else
+        if DEBUG
+            title(ax1,[" ", " "]) % for aligment
+        else
+            title([" ", " "]) % for aligment
+        end
+    end
+    
+    if DEBUG
+        % Select the top five predictions
+        [~,idx] = sort(score,'descend');
+        idx = idx(3:-1:1);
+        scoreTop = score(idx);
+        classNamesTop = string(classes(idx));
+    
+        % Plot the histogram
+        barh(ax2,scoreTop)
+        title(ax2,'Top 5')
+        xlabel(ax2,'Probability')
+        xlim(ax2,[0 1])
+        yticklabels(ax2, classNamesTop)
+        ax2.YAxisLocation = 'right';
+    end
 
     drawnow
-    % dodać bounding box
 end
+
+% turn off camera
+clear camera
