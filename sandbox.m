d14b08d39920bf84b77246aa0ec4c75463486a34
Trac3r <kamilg77@interia.pl>
2022-05-12 00:11:09 +0200

    Remove no longer needed files

--- sandbox.m
+++
@@ -1,9 +0,0 @@
-detector = yolov4ObjectDetector("tiny-yolov4-coco");
-disp(detector)
-analyzeNetwork(detector.Network)
-img = imread("highway.png");
-[bboxes,scores,labels] = detect(detector,img);
-detectedImg = insertObjectAnnotation(img,"Rectangle",bboxes,labels);
-imshow(img)
-figure
-imshow(detectedImg)
